ba56ef1c071c3134b29dc7ed3fe271d09ed92016
hampuslundblad <hampus.lundblad@hotmail.se>
2025-09-18 22:13:08 +0200

    fix llinting, update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,20 @@
-//  @ts-check
+// @ts-check
 
-import { tanstackConfig } from "@tanstack/eslint-config";
-import pluginQuery from '@tanstack/eslint-plugin-query'
+import { tanstackConfig } from "@tanstack/eslint-config"
+import pluginQuery from "@tanstack/eslint-plugin-query"
 
 export default [
-...pluginQuery.configs['flat/recommended'],
-...tanstackConfig]
+  ...tanstackConfig,
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+  },
+  // Optionally, ignore config files
+  {
+    ignores: ["eslint.config.js", "prettier.config.js", "vite.config.js"],
+  },
+]
